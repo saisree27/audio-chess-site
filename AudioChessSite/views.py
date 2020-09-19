087942a43282a14031c0a7f78d8771bcb3ca731e
Aryan34 <44515360+Aryan34@users.noreply.github.com>
2020-09-19 11:49:36 -0400

    Implement login and redirect to game page after logging in

--- AudioChessSite/views.py
+++ AudioChessSite/views.py
@@ -16,7 +16,7 @@ def login_request(request):
             if user is not None:
                 login(request, user)
                 messages.info(request, f"You are now logged in as {username}")
-                return redirect('/')
+                return redirect('/game')
             else:
                 messages.error(request, "Invalid username or password.")
         else:
