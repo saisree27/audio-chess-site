882d75a547ae4e0fe956cc68a15c49daf540cc05
saisree27 <saigautambonam@gmail.com>
2020-09-19 00:13:23 -0400

    Link templates and static files

--- AudioChessSite/settings.py
+++ AudioChessSite/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'AudioChessSite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR + '/AudioChessSite', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,5 +120,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),,
+    os.path.join(BASE_DIR + '/AudioChessSite', "static"),
 ]
