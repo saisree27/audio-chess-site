f6299ad700e5e6da6beea339d92f6aa753edd9f6
saisree27 <saigautambonam@gmail.com>
2020-09-19 16:49:13 -0400

    Re-add font-awesome to assets and change recording

--- AudioChessSite/static/js/recording.js
+++ AudioChessSite/static/js/recording.js
@@ -8,17 +8,17 @@ navigator.mediaDevices.getUserMedia({
     recorder.startRecording();
 
 
-    window.setInterval(function() {
-        recorder.pauseRecording();
-        let blob = recorder.getBlob();
-        invokeSaveAsDialog(blob);
-        recorder.resumeRecording();
-    }, 5000);
-    // const sleep = m => new Promise(r => setTimeout(r, m));
-    // await sleep(10000);
-
-    // recorder.stopRecording(function() {
+    // window.setInterval(function() {
+    //     recorder.pauseRecording();
     //     let blob = recorder.getBlob();
     //     invokeSaveAsDialog(blob);
-    // });
+    //     recorder.resumeRecording();
+    // }, 5000);
+    const sleep = m => new Promise(r => setTimeout(r, m));
+    await sleep(15000);
+
+    recorder.stopRecording(function() {
+        let blob = recorder.getBlob();
+        invokeSaveAsDialog(blob);
+    });
 });
