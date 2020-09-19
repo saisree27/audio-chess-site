4ac80ac9968b9e2140ac2913b4d1c93104ee16ac
saisree27 <saigautambonam@gmail.com>
2020-09-19 18:05:15 -0400

    Add csrf_token to chess.html

--- AudioChessSite/apps/base/views.py
+++ AudioChessSite/apps/base/views.py
@@ -2,6 +2,8 @@ from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, get_user_model
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from .forms import UploadFileForm
+
 
 def upload(request):
     if request.method == 'POST':
